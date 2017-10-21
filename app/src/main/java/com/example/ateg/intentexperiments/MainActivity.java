e5341bb9733cc872ea2aa8d91b4b3dd6450936e9
Adam Tegtmeier <ategtmei@gmail.com>
2017-10-20 23:00:03 -0400

    I thought that this would compile. The main project will, but not the tests.

--- app/src/main/java/com/example/ateg/intentexperiments/MainActivity.java
+++ app/src/main/java/com/example/ateg/intentexperiments/MainActivity.java
@@ -15,11 +15,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,69 +28,35 @@ import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.ateg.intentexperiments.FileSelector.FileOperation;
 import com.example.ateg.intentexperiments.FileSelector.FileSelector;
 import com.example.ateg.intentexperiments.FileSelector.OnHandleFileListener;
-import com.example.ateg.intentexperiments.FileSelector.SaveLoadClickListener;
 
 import java.io.File;
-import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseFragment<MainPresenter> implements MainView {
 
     private static final String TEXT_WINDOW_VALUE = "com.example.ateg.intentexperiments.TEXT_WINDOW_VALUE";
 
-    /**
-     * Sample filters array
-     */
     final String[] mFileFilter = {".txt", "*.*"};
-    //final String defaultLogFileName = "IntentExamination.txt";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
         if (savedInstanceState != null) {
             CharSequence textWindowValue = savedInstanceState.getCharSequence(TEXT_WINDOW_VALUE);
-            TextView textViewAs = (TextView) findViewById(R.id.central_textView);
+            TextView textViewAs = (TextView) getCreatedView().findViewById(R.id.central_textView);
             textViewAs.setText(textWindowValue);
         }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.action_button);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new ExamineServices(MainActivity.this).examineIntent(view, getIntent());
-            }
-        });
     }
 
     @Override
-    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onPostCreate(savedInstanceState, persistentState);
-        Intent in = getIntent();
-    }
-
-    @Override
-    public View onCreateView(String name, Context context, AttributeSet attrs) {
-        return super.onCreateView(name, context, attrs);
-
-        Boolean autoExamine = getSharedPreferences(SettingsOnAcceptListener.PREFERENCES_KEY, Context.MODE_PRIVATE)
-                .getBoolean(SettingsOnAcceptListener.AUTO_EXAMINE_KEY, false);
-        if (autoExamine) {
-            new ExamineServices(MainActivity.this).examineIntent(getCurrentFocus().getRootView(), getIntent());
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        getActivity().getMenuInflater().inflate(R.menu.menu_main, menu);
     }
 
     @Override
@@ -100,14 +65,13 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_settings:
 
-                final Dialog dialog = new Dialog(this);
+                final Dialog dialog = new Dialog(getActivity());
                 dialog.setContentView(R.layout.settings_dialog);
                 dialog.setTitle(R.string.settings_dialog_title);
                 dialog.setCancelable(true);
 
-
                 SharedPreferences sharedPreferences
-                        = getSharedPreferences(SettingsOnAcceptListener.PREFERENCES_KEY, MODE_PRIVATE);
+                        = getActivity().getSharedPreferences(SettingsOnAcceptListener.PREFERENCES_KEY, getActivity().MODE_PRIVATE);
 
                 CheckedTextView autoExamineCheckbox
                         = dialog.findViewById(R.id.settings_auto_examine_checkBox);
@@ -131,14 +95,14 @@ public class MainActivity extends AppCompatActivity {
                 defaultFileNameEditText.setText(defaultFileName);
 
                 Button acceptButton = (Button) dialog.findViewById(R.id.settings_accept_button);
-                acceptButton.setOnClickListener(new SettingsOnAcceptListener(this, dialog));
+                acceptButton.setOnClickListener(new SettingsOnAcceptListener(getActivity(), dialog));
 
                 Button resetButton = dialog.findViewById(R.id.settings_reset_button);
                 resetButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         SharedPreferences sharedPreferences
-                                = getSharedPreferences("IntentPreferences", MODE_PRIVATE);
+                                = getActivity().getSharedPreferences("IntentPreferences", getActivity().MODE_PRIVATE);
                         SharedPreferences.Editor editor = sharedPreferences.edit();
                         editor.clear();
                         editor.commit();
@@ -157,31 +121,31 @@ public class MainActivity extends AppCompatActivity {
 
                 return true;
             case R.id.save_to_file:
-                TextView textView = (TextView) findViewById(R.id.central_textView);
+                TextView textView = (TextView) getCreatedView().findViewById(R.id.central_textView);
                 File logFile = saveTextViewContentToFile(textView,
-                        LoggingUtilities.establishLogFile(this, Environment.DIRECTORY_DOCUMENTS));
+                        LoggingUtilities.establishLogFile(getActivity(), Environment.DIRECTORY_DOCUMENTS));
 
                 fileSaveCompleteSnackBar(textView, logFile);
 
                 return true;
             case R.id.save_to_file_as:
-                File[] files = ContextCompat.getExternalFilesDirs(this, Environment.DIRECTORY_DOCUMENTS);
+                File[] files = ContextCompat.getExternalFilesDirs(getActivity(), Environment.DIRECTORY_DOCUMENTS);
 
                 File storageDir = files[files.length - 1];
 
-                new FileSelector(this,
+                new FileSelector(getActivity(),
                         FileOperation.SAVE,
                         new OnHandleFileListener() {
                             @Override
                             public void handleFile(String filePath) {
-                                TextView textViewAs = (TextView) findViewById(R.id.central_textView);
+                                TextView textViewAs = (TextView) getCreatedView().findViewById(R.id.central_textView);
                                 File logFileAs = saveTextViewContentToFile(textViewAs, new File(filePath));
 
                                 fileSaveCompleteSnackBar(textViewAs, logFileAs);
                             }
                         },
                         mFileFilter,
-                        new File(storageDir, LoggingUtilities.getDefaultFileName(this)))
+                        new File(storageDir, LoggingUtilities.getDefaultFileName(getActivity())))
                         .show();
 
                 return true;
@@ -191,9 +155,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        TextView textViewAs = (TextView) findViewById(R.id.central_textView);
+        TextView textViewAs = (TextView) getCreatedView().findViewById(R.id.central_textView);
         outState.putCharSequence(TEXT_WINDOW_VALUE, textViewAs.getText());
     }
 
@@ -211,7 +175,7 @@ public class MainActivity extends AppCompatActivity {
 
     private File saveTextViewContentToFile(TextView textView, File destinationFile) {
         String examinationReport = textView.getText().toString();
-        LoggingUtilities loggingUtilities = new LoggingUtilities(this, destinationFile);
+        LoggingUtilities loggingUtilities = new LoggingUtilities(getActivity(), destinationFile);
         loggingUtilities.updateTextFile(examinationReport);
         return loggingUtilities.getLogFile();
     }
@@ -219,25 +183,79 @@ public class MainActivity extends AppCompatActivity {
     @NonNull
     private Intent intentToOpenFile(File tempFile) {
         Uri fileUri = FileProvider.getUriForFile(
-                getApplicationContext(),
+                getActivity(),
                 "com.example.ateg.intentexperiments.FileProvider",
                 tempFile);
 
         Intent intent = new Intent(Intent.ACTION_VIEW, fileUri);
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         intent.setDataAndType(fileUri,
-                getContentResolver().getType(fileUri));
+                getActivity().getContentResolver().getType(fileUri));
         return intent;
     }
 
     @NonNull
     private File copyToTempFile(File logFile) {
-        File tempFile = new File(getCacheDir(), "temp.txt");
+        File tempFile = new File(getActivity().getCacheDir(), "temp.txt");
 
-        new LoggingUtilities(getApplicationContext(), tempFile)
-                .updateTextFile(LoggingUtilities.readFile(getApplicationContext(), logFile));
+        new LoggingUtilities(getActivity(), tempFile)
+                .updateTextFile(LoggingUtilities.readFile(getActivity(), logFile));
 
         tempFile.deleteOnExit();
         return tempFile;
+    }
+
+    @Override
+    public void showError(Throwable ex) {
+
+    }
+
+    @Override
+    public void showLoading(Integer id) {
+
+    }
+
+    @Override
+    protected int layout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void setUi(View v) {
+
+        Toolbar toolbar = (Toolbar) v.findViewById(R.id.toolbar);
+
+        getActivity().setActionBar(toolbar);
+
+
+    }
+
+    @Override
+    protected void init() {
+    }
+
+    @Override
+    protected void populate() {
+        Boolean autoExamine = getActivity().getSharedPreferences(SettingsOnAcceptListener.PREFERENCES_KEY, Context.MODE_PRIVATE)
+                .getBoolean(SettingsOnAcceptListener.AUTO_EXAMINE_KEY, false);
+        if (autoExamine) {
+            new ExamineServices(getActivity()).examineIntent(getView(), getActivity().getIntent());
+        }
+    }
+
+    @Override
+    protected void setListeners() {
+        FloatingActionButton fab = (FloatingActionButton) getCreatedView().findViewById(R.id.action_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ExamineServices(getActivity()).examineIntent(view, getActivity().getIntent());
+            }
+        });
+    }
+
+    @Override
+    protected MainPresenter createPresenter() {
+        return new MainPresenter(this);
     }
 }
---
+++ app/src/main/java/com/example/ateg/intentexperiments/MainPresenter.java
@@ -0,0 +1,11 @@
+package com.example.ateg.intentexperiments;
+
+/**
+ * Created by ATeg on 10/20/2017.
+ */
+
+class MainPresenter extends BasePresenter<MainView>{
+    public MainPresenter(MainView viewInstance) {
+        super(viewInstance);
+    }
+}
